066398d19110cc5543f3d3dfc0e9dcc607e35858
Steven Haesler <hasv@zhaw.ch>
2025-05-23 10:33:05 +0200

    format

--- src/SpatialTaxonomy.py
+++ src/SpatialTaxonomy.py
@@ -1,6 +1,6 @@
 from threading import Thread
 from typing import List, Optional
-import xml.etree.ElementTree as ET, XMLParser
+import xml.etree.ElementTree as ET
 from threading import Thread
 from typing import List, Optional
 
